412bb1e731fd0298a1a0f0c736d4db0c7c06c3ed
akashwebdeveloper <akashdeveloper@protonmail.com>
2021-02-02 17:12:23 +0530

    solve error

--- controller/bazar/fullviewController.js
+++ controller/bazar/fullviewController.js
@@ -7,31 +7,42 @@ module.exports = {
     productfullview: (req, res) => {
         const { id } = req.body
 
-        Bazar.find({_id: id },['category', 'fullview', 'companyname', 'details.offerprice'], (err, datas) => {
+        Bazar.find({ _id: id }, ['category', 'fullview', 'companyname', 'details.offerprice'], (err, datas) => {
             // User.findOne({ email: email }, (err, users) => {
-                
-                if (err) {
-                    return res.status(502).json({ 
-                        success: false,
-                        status: 502,
-                        message: "err from database"
-                    })
-                }
 
-                if (!datas) {
-                    return res.status(202).json({
-                        success: false,
-                        status: 202,
-                        message: "No data in this Category"
-                    })
-                }
-                return res.status(200).json({
-                    success: true,
-                    status: 200,
-                    message: `data available in ${datas[0].category}`,
-                    data: [datas[0].fullview,datas[0].details]
+            if (err) {
+                return res.status(502).json({
+                    success: false,
+                    status: 502,
+                    message: "err from database"
                 })
-                
+            }
+
+            if (!datas) {
+                return res.status(202).json({
+                    success: false,
+                    status: 202,
+                    message: "No data in this Category"
+                })
+            }
+
+            
+            return res.status(200).json({
+                success: true,
+                status: 200,
+                message: `data available in ${datas[0].category}`,
+                data: {
+                    point: datas[0].fullview.point,
+                    productphoto: datas[0].fullview.productphoto,
+                    companyicon: datas[0].fullview.productphoto,
+                    companyname: datas[0].fullview.productphoto,
+                    discounttitle: datas[0].fullview.productphoto,
+                    discription: datas[0].fullview.productphoto,
+                    website: datas[0].fullview.productphoto,
+                    offerprice: datas[0].details.offerprice
+                }
             })
+
+        })
     },
 }
