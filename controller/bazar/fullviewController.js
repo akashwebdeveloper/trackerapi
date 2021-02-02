6f111378817bffdcb59efd9e4f34d4c089e65fa4
akashwebdeveloper <akashdeveloper@protonmail.com>
2021-02-02 14:39:06 +0530

    product info

--- controller/bazar/fullviewController.js
+++ controller/bazar/fullviewController.js
@@ -7,9 +7,8 @@ module.exports = {
     productfullview: (req, res) => {
         const { id } = req.body
 
-        Bazar.find({_id: id },['category', 'fullview', 'companyname'], (err, datas) => {
+        Bazar.find({_id: id },['category', 'fullview', 'companyname', 'details.offerprice'], (err, datas) => {
             // User.findOne({ email: email }, (err, users) => {
-                console.log(datas);
                 
                 if (err) {
                     return res.status(502).json({ 
@@ -26,12 +25,11 @@ module.exports = {
                         message: "No data in this Category"
                     })
                 }
-
                 return res.status(200).json({
                     success: true,
                     status: 200,
-                    message: `data available in ${datas[0].category}`,
-                    data: datas
+                    // message: `data available in ${datas[0].category}`,
+                    data: [datas[0].fullview,datas[0].details]
                 })
                 
             })
