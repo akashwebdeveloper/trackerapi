7ac5b8b08b251b3b94253e34f889556c45a68326
Akash kumar patel <akashclevbrain@gmail.com>
2021-03-10 18:11:21 +0530

    return joined true false

--- controller/users/challenges.js
+++ controller/users/challenges.js
@@ -1,5 +1,7 @@
 const Challenge = require('../../models/challenge')
+const User = require('../../models/user')
 const moment = require('moment');
+const admin = require('../../models/admin');
 const m = moment();
 module.exports = {
     getAllChallenges: (req, res) => {
@@ -74,44 +76,65 @@ module.exports = {
                             })
                         }
 
+                        Challenge.find({ _id: cid, joined: { $in: [uid] } }, ['_id'], (err, datas) => {
+                            // User.findOne({ email: email }, (err, users) => {
 
-                        const chall = [];
-                        const obj = {};
-                        obj._id = challenge._id
-                        obj.name = challenge.name
-                        obj.status = challenge.status
-                        obj.goal = challenge.goal
-                        obj.reward = challenge.reward
-                        obj.about = challenge.about
+                            if (err) {
+                                return res.status(502).json({
+                                    success: false,
+                                    status: 502,
+                                    message: "err from database"
+                                })
+                            }
 
-                        const challengeStart = moment(challenge.starttime)
-                        const challengeExpire = moment(challenge.expiretime)
+                            const chall = [];
+                            const obj = {};
 
-                        const days = challengeExpire.diff(challengeStart, 'days')
-                        const hours = challengeExpire.diff(challengeStart, 'hours') - (24 * (days));
-                        const minutes = (challengeExpire.diff(challengeStart, 'minutes') - (1440 * (days))) - (60 * hours);
+                            if (datas) {
+                                obj.joined = true
+                            }else {
+                                obj.joined = false
 
-
-                        var time = "";
-                        if (days > 0) {
-                            time += `${days} Days`;
-                        } else if (hours > 0) {
-                            time += `${hours} Hours ${minutes} Minutes `;
-                        } else {
-                            time += `${minutes} Minutes`;
-                        }
-
-                        obj.duration = time
-                        chall.push(obj)
+                            }
 
 
-                        return res.status(200).json({
-                            success: true,
-                            message: `Challenges details are here`,
-                            data: chall
+
+
+                            obj._id = challenge._id
+                            obj.name = challenge.name
+                            obj.status = challenge.status
+                            obj.goal = challenge.goal
+                            obj.reward = challenge.reward
+                            obj.about = challenge.about
+
+                            const challengeStart = moment(challenge.starttime)
+                            const challengeExpire = moment(challenge.expiretime)
+
+                            const days = challengeExpire.diff(challengeStart, 'days')
+                            const hours = challengeExpire.diff(challengeStart, 'hours') - (24 * (days));
+                            const minutes = (challengeExpire.diff(challengeStart, 'minutes') - (1440 * (days))) - (60 * hours);
+
+
+                            var time = "";
+                            if (days > 0) {
+                                time += `${days} Days`;
+                            } else if (hours > 0) {
+                                time += `${hours} Hours ${minutes} Minutes `;
+                            } else {
+                                time += `${minutes} Minutes`;
+                            }
+
+                            obj.duration = time
+                            chall.push(obj)
+
+
+                            return res.status(200).json({
+                                success: true,
+                                message: `Challenges details are here`,
+                                data: chall
+                            })
                         })
                     })
-
                 })
             } else {
                 Challenge.updateOne({ _id: challenge._id }, { $set: { startstatus: 'started' } }, (err) => {
@@ -127,29 +150,29 @@ module.exports = {
     joinchallenge: (req, res) => {
         const { uid, cid } = req.body
 
-        Challenge.findById(cid,['joined'], (err, result) =>{
+        Challenge.findById(cid, ['joined'], (err, result) => {
             if (err) throw err
-            if(result.joined.indexOf(uid) !== -1){
+            if (result.joined.indexOf(uid) !== -1) {
                 return res.status(202).json({
                     success: false,
                     status: 202,
                     message: "You have already joined this challenge",
                     data: result.joined
                 })
-            } else{
-                Challenge.findByIdAndUpdate(cid,{
-                    $push:{joined: uid}
-                },{
-                    new:true
-                }).exec((err,result)=>{
-                    if(err){
+            } else {
+                Challenge.findByIdAndUpdate(cid, {
+                    $push: { joined: uid }
+                }, {
+                    new: true
+                }).exec((err, result) => {
+                    if (err) {
                         return res.status(502).json({
                             success: false,
                             status: 502,
                             message: "err from database",
                             error: err
                         })
-                    }else{
+                    } else {
                         return res.status(200).json({
                             success: true,
                             status: 200,
