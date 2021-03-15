5c11a52aaa23a0c30c8ce117140fdf691dcba69a
Akash kumar patel <akashclevbrain@gmail.com>
2021-03-15 16:36:55 +0530

    verify changing

--- controller/auth/phoneController.js
+++ controller/auth/phoneController.js
@@ -225,7 +225,7 @@ function init(client) {
                                 message: "Number verified successfully",
                                 countryCode: country,
                                 phone,
-                                user: data
+                                user: data[0]
                             })
                         }
 
@@ -279,52 +279,6 @@ function init(client) {
                     })
                 })
         },
-        register1(req, res) {
-            const { countryCode, phone } = req.body
-
-            // User.findOne({ $or: [{ phone: phone }, { email: email }] }, (err, users) => {
-            User.findOne({ phone: phone }, (err, users) => {
-                if (err) {
-                    return res.status(502).json({
-                        success: false,
-                        status: 502,
-                        message: "err from database"
-                    })
-                }
-
-                if (users) {
-                    client
-                        .verify
-                        .services(serviceID)
-                        .verifications
-                        .create({
-                            to: `+${countryCode}${phone}`,
-                            channel: "sms"
-                        })
-                        .then((data) => {
-                            return res.status(200).json({
-                                success: true,
-                                message: "please check your mobile Number for otp verification",
-                                status: 200
-                            })
-                        }).catch((err) => {
-                            res.status(503).json({
-                                success: false,
-                                message: "please Enter correct country code/ mobile number",
-                                status: 503,
-                                err
-                            })
-                        })
-                } else {
-                    return res.status(202).json({
-                        success: true,
-                        status: 202,
-                        message: "Mobile number is not registered",
-                    })
-                }
-            })
-        }
-
     }
 }
 
